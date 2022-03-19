198dcc2caf6881f7f0bb8a3f880861534a4ac4f9
RomanV2 <101933459+RomanV2@users.noreply.github.com>
2022-03-19 19:05:38 +0300

    Create Ship.py

---
+++ Ship.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Ship():
+    
+    def __init__(self, screen):
+        """Инициализирует корабль и задает его начальную позицию """
+        self.screen = screen
+        
+        # загрузка изображения корабля и получение прямоугольника 
+        self.image = pygame.image.load('images/rocket.bmp')
+        self.rect = self.image.get.rect()
+        self.screen_rect = screen.get_rect()
+        
+        # каждый новый корабль появляется у нижнего края экрана
+        self.rect.centerx = self.sсreen_rect.centerx
+        self.rect.bottom = self.sсreen_rect.bottom
+        
+    def blime(self):
+        """Рисует корабль в текущей позиции"""
+        self.screen.blit(self.image, self.rect)
+        
