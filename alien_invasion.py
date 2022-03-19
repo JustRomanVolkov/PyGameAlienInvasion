fdd00fe75912148499905ed500a4bf43535b7534
RomanV2 <101933459+RomanV2@users.noreply.github.com>
2022-03-19 18:06:31 +0300

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,17 +18,21 @@ import sys
 import pygame
 
 def run_game():
-  # инициализирует игру и создает объект экрана
-  pygame.init()
-  screen = pygame.display.set_mode((1200, 800))
-  pygame.display.set_caption("Alien Invasion")
+    # инициализирует игру и создает объект экрана
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    # назначение цвета фона
+    bg_color = (230, 230, 230)
   
-  # запуск основного цикла игры
-  while True:
-    # отслеживание событий клавиатуры и мыши
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT():
-        sys.exit()
+    # запуск основного цикла игры
+    while True:
+        # отслеживание событий клавиатуры и мыши
+        for event in pygame.event.get():
+            # перерисовка экрана
+            screen.fill(bg_color)
+            if event.type == pygame.QUIT():
+            sys.exit()
         
     # отображение последнего прорисованного экрана
     pygame.display.flip()
