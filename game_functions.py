ea242303c6b4a836858c0e56e6b6ed5010f43cfc
RomanV2 <101933459+RomanV2@users.noreply.github.com>
2022-03-20 19:05:35 +0300

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -6,3 +6,12 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT():
             sys.exit()
+            
+def update_screen(ai_settings, screen, ship):
+    """Обновляет изображения на экране и отображает новый экран."""
+    # перерисовка экрана
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+        
+    # отображение последнего прорисованного экрана
+    pygame.display.flip()
