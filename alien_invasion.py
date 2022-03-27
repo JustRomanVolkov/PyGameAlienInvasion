c89a04d09bdc3e8a66be94a9398dbbbb2e30f1ff
RomanV2home <volkovr_vk@mail.ru>
2022-03-27 09:52:24 +0300

    TIK-001_удаление пуль с экрана

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,11 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+        # удаление пуль за экраном
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 
