ae20c8e5a202e0879405c66bd1b0ce5665e251b2
RomanV2home <volkovr_vk@mail.ru>
2022-03-27 11:20:47 +0300

    HOTFIX исправил ошибку отображения перемещения вправо добавил выход на 'q'

--- game_functions.py
+++ game_functions.py
@@ -15,6 +15,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_SPACE:
         # создание пули и включение ее в группу
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 
 def check_keyup_events(event, ship):
