70dd10bbad0e854ccbb5875faf9e50178b6b41aa
RomanV2home <volkovr_vk@mail.ru>
2022-03-28 22:35:08 +0300

    добавил первый ряд НЛО, вместо одного

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings():
         """Инициализирует настройки игры"""
         # Параметры экрана
         self.screen_width = 1200
-        self.screen_height = 500
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
         # настройки корабля
         self.ship_speed_factor = 1.5
