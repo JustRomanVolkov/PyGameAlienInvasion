79c74835f647e7f8a1682405da0372f8d4e3e53e
RomanV2home <volkovr_vk@mail.ru>
2022-03-31 22:39:42 +0300

    обработка коллизии при столкновении НЛО и корабля

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from time import sleep
+
 from bullet import Bullet
 from alien import Alien
 
@@ -83,7 +84,7 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-            check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
