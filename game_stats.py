3dd338fd19ee8f708cb772a9cd60753111bfadd3
RomanV2home <volkovr_vk@mail.ru>
2022-04-03 08:40:28 +0300

    вывод на экран ноормера уровня + fix

--- game_stats.py
+++ game_stats.py
@@ -22,4 +22,3 @@ class GameStats():
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
-
--- scoreboard.py
+++ scoreboard.py
@@ -69,5 +69,3 @@ class Scoreboard():
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
-
-
