0d5773e9f44e365b987d76e9f4e082cfa250c5b2
RomanV2home <volkovr_vk@mail.ru>
2022-04-03 08:36:15 +0300

    вывод на экран ноормера уровня

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,7 +32,7 @@ def run_game():
     # запуск основного цикла игры
     while True:
         # отслеживание событий клавиатуры и мыши
-        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
         if stats.game_active:
             ship.update()
